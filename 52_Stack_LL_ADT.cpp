5918771069c4834b9070b071314d5968c210b249
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2024-05-20 20:46:21 +0500

    Made Stack data type Generic

--- 52_Stack_LL_ADT.cpp
+++ 52_Stack_LL_ADT.cpp
@@ -1,83 +1,88 @@
 #include <iostream>
 
+
+template <typename T>
 class Node {
 public:
-    int data;
+    T data;
     Node* next;
-    Node() {}
-    Node(int data) :data(data), next(nullptr) {}
+    Node(T data) :data(data), next(nullptr) {}
+
 };
 
+template <typename T>
 class StackLLADT {
-    Node* top;
+    Node<T>* top;
 public:
+
     StackLLADT() :top(nullptr) {}
-    void push(int x);
-    int pop();
-    int peek(int index);
-    int stackTop();
-    bool isEmpty();
-    bool isFull();
-    void display();
+
+    ~StackLLADT() {
+        top = nullptr;
+        delete top;
+    }
+
+    void push(T x) {
+        Node<T>* newNode = new Node(x);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    T pop() {
+        Node<T>* temp = top;
+        top = top->next;
+        int poppedData = temp->data;
+        delete temp;
+        return poppedData;
+    }
+
+    T peek(int index) {
+        Node<T>* temp = top;
+        for (int i = 0; i < index; i++) {
+            if (temp == nullptr) return -1;
+            temp = temp->next;
+        }
+        return temp == nullptr ? -1 : temp->data;
+    }
+
+    T stackTop() {
+        return isEmpty() ? -1 : top->data;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    bool isFull() {
+        Node<T>* node = new Node(0);
+        // Node creation failed
+        return node == nullptr;
+    }
+
+    void display() {
+        Node<T>* temp = top;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
-void StackLLADT::push(int x) {
-    Node* newNode = new Node(x);
-    newNode->next = top;
-    top = newNode;
-}
 
-int StackLLADT::pop() {
-    Node* temp = top;
-    top = top->next;
-    int poppedData = temp->data;
-    delete temp;
-    return poppedData;
-}
 
-int StackLLADT::peek(int index) {
-    Node* temp = top;
-    for (int i = 0; i < index; i++) {
-        if (temp == nullptr) return -1;
-        temp = temp->next;
-    }
-    return temp == nullptr ? -1 : temp->data;
-}
 
-int StackLLADT::stackTop() {
-    return isEmpty() ? -1 : top->data;
-}
-
-bool StackLLADT::isEmpty() {
-    return top == nullptr;
-}
-
-bool StackLLADT::isFull() {
-    Node* node = new Node();
-    // Node creation failed
-    return node == nullptr;
-}
-
-void StackLLADT::display() {
-    Node* temp = top;
-    while (temp != nullptr) {
-        std::cout << temp->data << " ";
-        temp = temp->next;
-    }
-    std::cout << std::endl;
-}
-
-int main() {
-    StackLLADT st;
-    std::cout << st.isEmpty() << std::endl;
-    st.push(1);
-    st.push(2);
-    st.push(3);
-    std::cout << st.isEmpty() << std::endl;
-    st.push(4);
-    st.push(5);
-    st.display();
-    std::cout << st.stackTop() << std::endl;
-    std::cout << st.isFull() << std::endl;
-    std::cout << st.peek(4) << std::endl;
-}
+// int main() {
+//     StackLLADT<int> st;
+//     std::cout << st.isEmpty() << std::endl;
+//     st.push(1);
+//     st.push(2);
+//     st.push(3);
+//     std::cout << st.isEmpty() << std::endl;
+//     st.push(4);
+//     st.push(5);
+//     st.display();
+//     std::cout << st.stackTop() << std::endl;
+//     std::cout << st.isFull() << std::endl;
+//     std::cout << st.peek(4) << std::endl;
+// }
